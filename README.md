a41cfb258e132a9e4bcde077f0ccfc7c92b27a43
lq <max.li@madv.com>
2021-02-08 15:07:27 +0800

    新增云存api

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # 阿里生活物联网平台（飞燕平台）sdk
 
+介绍
+
+该composer包，封装阿里物联网平台，云端api接口。可以对比[阿里云文档](https://help.aliyun.com/document_detail/153142.html?spm=a2c4g.11186623.3.3.5bf86cb8Piet1f)，查看参数描述。
+
 安装
 ```
 composer require yun-hai/living-ali
@@ -12,6 +16,16 @@ composer require yun-hai/living-ali
  //查询免费的云存储套餐详情
  $meal->getFreecloudstorage($iotid);
 ```
+返回数据正常示例：
+
+```
+[
+    'code' =>  200,
+    'data' => [
+    ]
+]
+```
+
 
 文档
 
@@ -38,7 +52,39 @@ composer require yun-hai/living-ali
  $meal->queryCommodity($iotId)
 ```
 
+4.查询云存储套餐是否可以购买
 
+``` 
+$meal->checkCommodity($iotId, $commodityCode, $specification)
+```
+
+5.获取云存储订单详情
+
+```
+$meal->getOrder($iotId, $orderId)
+```
+
+6.查询云存储套餐的订单列表
+
+```
+$meal->queryOrder($iotId, $pageNo = 0, $pageSize = 50)
+```
+
+7.设置免费云存储套餐立即生效
+
+```
+$meal->enableFreecloudstorage($iotId)
+```
+
+8.设置云存储套餐立即生效
+```
+$meal->enableCommodity($iotId)
+```
+
+9.设置用户的云存储套餐状态
+```
+$meal->setStatus($iotId, $orderId, $status)
+```
 
 License
 
--- src/Meal.php
+++ src/Meal.php
@@ -26,40 +26,154 @@ class Meal
     }
 
     /**
-     * 调用该接口查询免费的云存储套餐详情。
+     * 查询免费的云存储套餐详情。
      *
      * @param string $iotId
+     * @return array
      */
-    public function getFreecloudstorage(string $iotId)
+    public function getFreecloudstorage($iotId)
     {
-        $response = $this->client->post('/vision/customer/freecloudstorage/get', ['iotId' => $iotId], "1.0.1");
+        $response = $this->client->post(
+            '/vision/customer/freecloudstorage/get', ['iotId' => $iotId], "1.0.1"
+        );
 
         return $response;
     }
 
     /**
-     * 调用该接口领取免费的云存储套餐。
+     * 领取免费的云存储套餐。
+     *
+     * @param string $iotId
+     * @return array
      */
-    public function consumeFreecloudstorage()
+    public function consumeFreecloudstorage($iotId)
     {
-        $client = new Client();
-        $client->post('/vision/customer/freecloudstorage/consume', ['iotId' => $iotId], "1.0.1");
+        $response = $this->client->post(
+            '/vision/customer/freecloudstorage/consume', ['iotId' => $iotId], "1.0.4"
+        );
+
+        return $response;
     }
 
 
     /**
-     * 调用该接口查询视频云存储套餐列表。
+     * 查询视频云存储套餐列表。
+     *
+     * @param string $iotId
+     * @return array
      */
-    public function queryCommodity()
+    public function queryCommodity($iotId)
     {
+        $response = $this->client->post(
+            '/vision/customer/cloudstorage/commodity/query', ['iotId' => $iotId], "1.0.4"
+        );
 
+        return $response;
     }
 
     /**
-     * 调用该接口查询云存储套餐是否可以购买。
+     * 查询云存储套餐是否可以购买。
+     *
+     * @param string $iotId
+     * @param string $commodityCode
+     * @param string $specification
+     * @return array
      */
-    public function checkCommodity()
+    public function checkCommodity($iotId, $commodityCode, $specification)
     {
+        $response = $this->client->post(
+            '/vision/customer/cloudstorage/commodity/check',
+            ['iotId' => $iotId, 'commodityCode' => $commodityCode, 'specification' => $specification],
+            "1.0.2"
+        );
 
+        return $response;
     }
+
+    /**
+     * 获取云存储订单详情
+     *
+     * @param string $iotId
+     * @param  string $orderId
+     * @return  array
+     */
+    public function getOrder($iotId, $orderId)
+    {
+        $response = $this->client->post(
+            '/vision/customer/cloudstorage/order/get',
+            ['iotId' => $iotId, 'orderId' => $orderId],
+            "1.0.4"
+        );
+
+        return $response;
+    }
+
+    /**
+     * 查询云存储套餐的订单列表
+     *
+     * @param $iotId
+     * @param int $pageNo
+     * @param int $pageSize
+     * @return array
+     */
+    public function queryOrder($iotId, $pageNo = 0, $pageSize = 50)
+    {
+        $response = $this->client->post(
+            'vision/customer/cloudstorage/order/query',
+            ['iotId' => $iotId, 'pageNo' => $pageNo, 'pageSize' => $pageSize],
+            "1.0.1"
+        );
+
+        return $response;
+    }
+
+
+    /**
+     * 设置免费云存储套餐立即生效
+     *
+     * @param  string $iotId
+     * @return array
+     */
+    public function enableFreecloudstorage($iotId)
+    {
+        $response = $this->client->post(
+            '/vision/customer/freecloudstorage/enable',
+            ['iotId' => $iotId],
+            "1.0.1"
+        );
+
+        return $response;
+    }
+
+    /**
+     * 设置云存储套餐立即生效
+     *
+     * @param  string $iotId
+     * @return array
+     */
+    public function enableCommodity($iotId)
+    {
+        $response = $this->client->post('/vision/customer/cloudstorage/commodity/enable', [
+            'iotId' => $iotId], "1.0.1");
+
+        return $response;
+    }
+
+    /**
+     * 设置用户的云存储套餐状态
+     *
+     * @param $iotId
+     * @param $orderId
+     * @param $status
+     * @return array
+     */
+    public function setStatus($iotId, $orderId, $status)
+    {
+        $response = $this->client->post('/vision/customer/cloudstorage/status/set', [
+            'iotId' => $iotId, 'orderId' => $orderId, 'status' => $status
+        ], "1.0.1");
+
+        return $response;
+    }
+
 }
